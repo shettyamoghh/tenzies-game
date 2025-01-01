b2c4d69adc4af0d58cb08f0476d64689a3b1248f
Amogh Shetty <amoggo2004@gmail.com>
2025-01-01 18:44:44 +0300

    Implement dice and random die generation

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import Die from './components/Die'
+import { nanoid } from 'nanoid'
+
+function App() {
+  // set/update array of dice
+  const [dice, setDice] = useState(generateNewDice())
+  
+  // throw die (randomDie)
+  function randomDie() {
+    return Math.ceil(Math.random()*6)
+  }
+
+  // generate array with random dice
+  function generateNewDice() {
+    return new Array(10)
+      .fill(0)
+      .map(() => ({
+        value: randomDie(),
+        isHeld: false,
+        id: nanoid()
+      }))
+  }
+
+  const diceElements = dice.map(die => (
+    <Die key={die.id} value={die.value}/>
+  ))
+
+  return (
+    <main>
+      <div className='dice-container'>
+        {diceElements}
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Die.jsx
@@ -0,0 +1,7 @@
+function Die(props) {
+    return (
+        <button>{props.value}</button>
+    )
+}
+
+export default Die

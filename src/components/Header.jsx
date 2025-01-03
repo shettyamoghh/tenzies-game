19562c77d732a2d3cfbbdd086aef1cee7fe4ed21
Amogh Shetty <amoggo2004@gmail.com>
2025-01-03 17:24:13 +0300

    Update header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <header>
             <h1 className="title">Tenzies</h1>
-            <p className="instructions">Keep rolling until all dice are the same! Click on each die to freeze its current number</p>
+            <p className="instructions">Click on each die to freeze its current number, and keep rolling until all dice are the same!</p>
         </header>
     )
 }
